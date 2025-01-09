e2b3a52c901e505b2f2c089ce0f9c3b525fd9258
Methre Amarnath <methreamarnath@gmail.com>
2025-01-10 01:25:25 +0530

    Update index.js to redirect to gallery

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Home() {
-  return <h1>Hello, Next.js!</h1>;
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to the gallery app
+    router.push('/gallery');
+  }, [router]);
+
+  return null; // Return null as the component will redirect immediately
 }
